ce3c73900e69ccdcf1900d1ee900f749005e4321
Louis Mazza <louismazza@gmail.com>
2018-01-06 08:35:20 -0800

    initial commit

---
+++ blogPostRouter.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+
+// we're going to add some items to BlogPosts
+// so there's some data to look at
+BlogPosts.create('A Good Day', 'it was pretty good today.', 'Gil Fellows', 2017);
+BlogPosts.create('Rock and Roll!', 'i like music.', 'Tad Smurthy', 2014);
+BlogPosts.create('Depressions', 'everything is terrible.', 'Cog Rockman', 2016);
+
+// when the root of this router is called with GET, return
+// all current BlogPosts items
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `BlogPosts.update` with updated item.
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    author: req.body.author,
+    title: req.body.title,
+    content: req.body.content
+  });
+  res.status(204).end();
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from BlogPosts.
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+
+//export
+module.exports = router
